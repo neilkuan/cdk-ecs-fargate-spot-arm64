1a96b22994018f270f32d44e8efcf3f0a4910f0e
Neil Kuan <guan840912@gmail.com>
2024-09-07 10:04:01 +0800

    first commit

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,7 +2,7 @@ import { awscdk } from 'projen';
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
-  name: 'ecs-fargate-spot-arm64',
+  name: 'cdk-ecs-fargate-spot-arm64',
   projenrcTs: true,
 
   // deps: [],                /* Runtime dependencies of this module. */
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,40 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecsp from 'aws-cdk-lib/aws-ecs-patterns';
 import { Construct } from 'constructs';
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
-
-    const vpc = new ec2.Vpc(this, 'ecsDemo', { maxAzs: 2, natGateways: 1 });
-    const cluster = new ecs.Cluster(this, 'democluster', {
-      clusterName: 'democluster',
-      vpc,
-      defaultCloudMapNamespace: {
-        name: 'demo',
-      },
-    });
-    const mocktaskDefinition = new ecs.FargateTaskDefinition(this, 'mocktaskDefinition', {
+    const vpc = new ec2.Vpc(this, 'ecsDemo', { maxAzs: 2, natGateways: 0 });
+    const cluster = new ecs.Cluster(this, 'democluster', { clusterName: 'democluster', vpc });
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'taskDefinition', {
       cpu: 256,
       memoryLimitMiB: 512,
+      runtimePlatform: { operatingSystemFamily: ecs.OperatingSystemFamily.LINUX, cpuArchitecture: ecs.CpuArchitecture.ARM64 },
     });
+    taskDefinition.addContainer('nginx', { image: ecs.ContainerImage.fromRegistry('public.ecr.aws/nginx/nginx:1.27-arm64v8'), portMappings: [{ containerPort: 80 }] });
+    new ecsp.ApplicationLoadBalancedFargateService(this, 'api', {
+      cluster,
+      assignPublicIp: true,
+      listenerPort: 80,
+      cpu: 256,
+      memoryLimitMiB: 512,
+      capacityProviderStrategies: [
+        {
+          capacityProvider: 'FARGATE_SPOT', weight: 100,
+        },
+      ],
+      taskDefinition,
+      taskSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      serviceName: 'nginx',
+      publicLoadBalancer: true,
+      desiredCount: 1,
+    });
+
 
   }
 }
